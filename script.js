5eee2878bad814cefc8b39cc9abc7031926560d4
yasin <127941127+yasin1379mo@users.noreply.github.com>
2023-11-04 19:25:24 +0330

    step2 = delete option

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const itemForm = document.querySelector("#item-form");
 const itemInput = document.querySelector("#item-input");
 const itemList = document.querySelector("#item-list");
 const ul = document.querySelectorAll("ul");
+const clear = document.querySelector("#clear");
+const filter = document.querySelector("#filter");
 
 function addItem(e) {
   e.preventDefault();
@@ -17,11 +19,45 @@ function addItem(e) {
   //create list item
   const li = document.createElement("li");
   li.innerHTML = `${newItem} 
-<button class="remove-item btn-link text-red">
-<i class="fa-solid fa-xmark"></i>
+  <button class="remove-item btn-link text-red">
+  <i class="fa-solid fa-xmark"></i>
 </button>`;
   itemList.appendChild(li);
+  checkUI();
+  itemInput.value = "";
+}
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure you want to remove?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+  checkUI();
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+
+  if (items.length === 0) {
+    clear.style.display = "none";
+    filter.style.display = "none";
+  } else {
+    clear.style.display = "block";
+    filter.style.display = "block";
+  }
 }
 
 // event listener
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clear.addEventListener("click", clearItems);
+
+checkUI();
